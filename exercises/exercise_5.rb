174caf240ff70b70d3a0fab64447a8995784549e
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-12-12 15:58:41 -0800

    Implements  exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum_revenue = Store.sum(:annual_revenue)
+@average_revenue = @sum_revenue / Store.count
+
+@revenue_over_1M = Store.where("annual_revenue > 1000000").count
+
+
+puts @sum_revenue
+puts @average_revenue
+puts @revenue_over_1M
