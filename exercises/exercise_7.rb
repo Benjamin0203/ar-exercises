b4a81446a1339859c34ac1ccad45ad5c721145d9
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-12-12 16:59:49 -0800

    Implements exercise_7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please input a store name"
+print ">"
+
+@store_name = gets.chomp
+
+add_name = Store.create(name: @store_name)
+
+
+puts "Error: #{add_name.errors.full_messages}"
